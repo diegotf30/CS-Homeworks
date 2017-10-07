338b380c4d50f00e11f009516f2f243ee3c68eb1
Diego Trevino <diegotf30@gmail.com>
2017-10-07 12:42:16 -0500

    RegularPyramid Finished

--- RegularPyramid/main.cpp
+++ RegularPyramid/main.cpp
@@ -1,9 +1,17 @@
+///Programmer: Diego Treviño.
+///This program covers the subject of Class Composition, given a
+///		regular pyramid (with an equilateral triangular base) it
+///		calculates its superficial area.
+
 #include <iostream>
+
+#include "RegularPyramid.h"
 
 using namespace std;
 
 int main()
 {
-    cout << "Hello world!" << endl;
-    return 0;
+    RegularPyramid pyramid(Triangle(5,2), 5, 2);
+
+    cout << "Superficial Area of the Pyramid is: " << pyramid.calcArea() << endl;
 }
